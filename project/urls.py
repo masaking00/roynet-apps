b6c20f023696a7eab43e0049156f887b17f0b612
masa <masa016@gmail.com>
2021-09-08 00:44:01 +0900

    no message

--- project/urls.py
+++ project/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('register.urls')),
     path('report/',include('report.urls')),
 ]  +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
